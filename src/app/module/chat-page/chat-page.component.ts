32dd872325d5a6f9fa3fe834fef22f8e185dba39
gustavodesousalima <gustavdesousalima@gmail.com>
2025-02-17 08:53:53 -0300

    Update

--- src/app/module/chat-page/chat-page.component.ts
+++ src/app/module/chat-page/chat-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../components/side-bar/side-bar.component';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Component({
   selector: 'app-chat-page',
@@ -11,11 +12,25 @@ import { ActivatedRoute } from '@angular/router';
 export class ChatPageComponent {
   conversationid!: number;
   userhistory!: boolean;
+  response!: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.params.subscribe(params => {
       this.conversationid = params['conversationid'];
       this.userhistory = params['userhistory'];
+    });
+
+    //requisição get para api e guardar a resposta em uma variável
+
+    //requisição post para api
+    const req = new HttpRequest('GET', 'https://api.example.com', {
+      params: {
+        conversationid: this.conversationid,
+        userhistory: this.userhistory
+      }
+    });
+    this.http.request(req).subscribe(response => {
+      this.response = response;
     });
   }
 }
